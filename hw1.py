50f26e31edff71b119c93970926dd1ff581ebc94
Cayenne007 <46312967+Cayenne007@users.noreply.github.com>
2023-02-11 10:44:25 +0300

    hw1

--- hw1.py
+++ hw1.py
@@ -2,35 +2,35 @@
 
 
 #Задача 1
-# def plus(*text):
-#     result = 0
-#     for num in text:
-#         result += int(num)
-#     return result
+def plus(*text):
+    result = 0
+    for num in text:
+        result += int(num)
+    return result
 
-# text = input('Введите трехзначное число : ')
-# print('Результат: ', plus(text[0], text[1], text[2]))
+text = input('Введите трехзначное число : ')
+print('Результат: ', plus(text[0], text[1], text[2]))
 
 #Задача 2
-# s = int(input('Введите общее количество журавликов: '))
-# if s%6 != 0:
-#     print('Указано некорректное число')
-# else:  
-#     x = int(s/6) 
-#     print('Петя и Сережа сделали по: ',x,', а Катя сделала: ',s-2*x)
+s = int(input('Введите общее количество журавликов: '))
+if s%6 != 0:
+    print('Указано некорректное число')
+else:  
+    x = int(s/6) 
+    print('Петя и Сережа сделали по: ',x,', а Катя сделала: ',s-2*x)
 
 #Задача 3
-# def isLucky(number):
-#     if len(number) != 6:
-#         return False
+def isLucky(number):
+    if len(number) != 6:
+        return False
     
-#     return plus(number[0],number[1],number[2]) == plus(number[-1],number[-2],number[-3])
+    return plus(number[0],number[1],number[2]) == plus(number[-1],number[-2],number[-3])
 
-# number = input('Введите номер вашего билета: ')
-# if isLucky(number):
-#     print('Ура! Ваш билет счастливый!')
-# else:
-#     print('У вас обычный билет. Повезет в следующий раз')
+number = input('Введите номер вашего билета: ')
+if isLucky(number):
+    print('Ура! Ваш билет счастливый!')
+else:
+    print('У вас обычный билет. Повезет в следующий раз')
 
 
 #Задача 4
